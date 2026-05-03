f652d7c07bf522955dec890153b9173e95b47ab0
Ganesh Srinivasan <ganesh.srinivasan@servicenow.com>
2026-05-03 09:53:39 -0400

    debug: show audio file info before transcribing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -89,7 +89,11 @@ export default function TodayScreen() {
       await recordingRef.current.stopAndUnloadAsync();
       const uri = recordingRef.current.getURI();
       recordingRef.current = null;
-      if (uri) await transcribeAudio(uri);
+      if (uri) {
+        const info = await FileSystem.getInfoAsync(uri);
+        Alert.alert("Audio file", `URI: ${uri}\nSize: ${JSON.stringify(info)}`);
+        // await transcribeAudio(uri);
+      }
     } catch (e) {
       console.error(e);
     } finally {
